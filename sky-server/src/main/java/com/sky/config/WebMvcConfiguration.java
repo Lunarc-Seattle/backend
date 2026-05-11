cf0e243de4ca74a9a16436c59a856f8b5e8bf86c
JIE （FIA） YIN <994527498@qq.com>
2026-05-11 15:19:06 -0700

    1

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -14,6 +15,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 /**
  * 配置类，注册web层相关组件
@@ -67,5 +70,15 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始静态资源映射。。。");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * 
+     * @param converters the list of configured converters to extend
+     */
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.extendMessageConverters(converters);
+        //不知道的可以去看看雷神的boot3,里面有源码分析
+        //@DateTimeFormat用于接受参数时（DTO）将字符串转换为日期时间类型，@JsonFormat用于返回数据时（entity）将日期时间类型格式化为字符串
     }
 }
